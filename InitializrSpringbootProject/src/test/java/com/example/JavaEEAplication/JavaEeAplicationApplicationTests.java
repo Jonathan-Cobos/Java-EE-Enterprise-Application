79a35895f89def5a3bc924facd032a867fd48c10
Jonathan-Cobos <128739751+Jonathan-Cobos@users.noreply.github.com>
2025-12-30 19:51:26 -0500

    Add files via upload

---
+++ InitializrSpringbootProject/src/test/java/com/example/JavaEEAplication/JavaEeAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaEEAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEeAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
